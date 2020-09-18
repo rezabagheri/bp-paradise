6821490a39aff34b9e0dbe3f3f66aa990e00da6e
reza bagheri <rezabagheri@gmail.com>
2020-09-18 21:59:54 +0430

    Use Wordpress language attrib , charset and title

--- header.php
+++ header.php
@@ -1,9 +1,10 @@
-<html lang="en">
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?= bloginfo('charset') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?= bloginfo('title'); ?> </title>
 </head>
 
 <body>

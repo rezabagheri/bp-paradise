905df7d49f1c571238ac1b9cc1de5398653e4b3e
reza bagheri <rezabagheri@gmail.com>
2020-09-22 22:39:07 +0330

    Add Bootstrap5 styles and scripts

--- inc/Setup/Enqueue.php
+++ inc/Setup/Enqueue.php
@@ -6,17 +6,20 @@ class Enqueue
 {
     public function register()
     {
-        add_action('wp_enqueue_styles', [$this, 'addStyles'] );
+        add_action( 'wp_enqueue_scripts', [$this, 'addStyles'] );
         add_action( 'wp_enqueue_scripts', [$this, 'addScripts'] );
     }
 
     public function addStyles()
     {
         wp_enqueue_style( "theme-css", get_template_directory_uri() . "/assets/css/style.css", [], "1.0", "all" );
+        wp_enqueue_style( "theme-css1", get_template_directory_uri() . "/assets/css/bootstrap.min.css", [], "1.0", "all" );
     }
 
     public function addScripts()
     {
         wp_enqueue_script( "theme-scripts", get_template_directory_uri() . "/assets/js/scripts.js", [ 'jquery' ], "1.0", true );
+        wp_enqueue_script( "popper-scripts", get_template_directory_uri() . "/assets/js/popper.js", [ 'jquery' ], "1.0", true );
+        wp_enqueue_script( "bootstrap-scripts", get_template_directory_uri() . "/assets/js/bootstrap.min.js", [ 'jquery' ], "1.0", true );
     }
 }
